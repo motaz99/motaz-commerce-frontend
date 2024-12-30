33ceb0fce22a61a2c64b4dc70656b0660b68a467
motaz99 <motaz.dn.ali@gmail.com>
2024-12-30 19:05:31 +0300

    fix: replace the name of dashboard component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-export default function EmployeeList() {
+export default function Dashboard() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold text-gray-300">
